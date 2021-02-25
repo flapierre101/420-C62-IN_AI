4ca9e2f2275d980d40ff7dff8fc317a3b506cc79
Dany Viens <dany.viens@gmail.com>
2021-02-25 09:14:27 -0500

    Modif de leastsquare

--- src/recherche.py
+++ src/recherche.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def produitScalaire(motA, motB):  # motA & motB = type np.array(1,y)
     # self.result = np.sum(motA * motB)
     return np.sum(motA * motB)
@@ -6,7 +8,8 @@ def produitScalaire(motA, motB):  # motA & motB = type np.array(1,y)
 def leastSquares(motA, motB):  # motA & motB = type np.array(1,y)
     # donne le least square entre 2 mots
     # return np.sum(motA**2 - motB**2)
-    return np.sum(np.square(motA) - np.square(motB))
+    # return np.sum(np.square(motA) - np.square(motB))
+    return np.sum(np.square(motA - motB))
 
 
 def comparator(lq1, lq2):  # Entier resultat optenu de la methode leastSquare ou cityBlock
